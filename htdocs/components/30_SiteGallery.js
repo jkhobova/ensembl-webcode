22dd739688375147754b549c74569dae0c0a134d
Anne Lyle <annelyle@ebi.ac.uk>
2015-10-01 12:33:25 +0100

    Added function to auto-populate identifier field

--- htdocs/components/30_SiteGallery.js
+++ htdocs/components/30_SiteGallery.js
@@ -23,6 +23,23 @@ Ensembl.Panel.SiteGalleryHome = Ensembl.Panel.Content.extend({
     var panel = this;
     this.base();
 
+    // Auto-populate identifier field with hidden value, based on species and feature type selected
+    this.elLk.radio_var   = $('input:radio[name=data_type_var]', this.el);
+    //this.elLk.radio_novar = $('input:radio[name=data_type_novar]', this.el);
+    panel.elLk.select     = $('select[name=species]', this.el);
+    panel.elLk.identifier = $('input[name=identifier]', this.el);
+
+    this.elLk.radio_var.on({
+      'change': function() {
+        var type    = $(this).val();
+        var species = panel.elLk.select.val();
+        panel.elLk.example = $('input[name='+species+'-'+type+']');
+        var example = panel.elLk.example.val();
+        panel.elLk.identifier.val(example);
+      }
+    });
+
+    // Add species name at beginning of form URL e.g. /Homo_sapiens/Info/GeneGallery
     $('form.add_species_on_submit').on('submit', function () {
       var form        = this;
       var old_action  = $(form).attr('action');
